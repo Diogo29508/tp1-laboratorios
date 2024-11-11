b468bebec5e14e55f0266e62cd6cb3bbb616e682
JoseFerSa <a30313@alunos.ipca.pt>
2024-11-11 12:47:56 +0000

    Codigo atualizado C

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
-#define led_pin PB5
+#define F_CPU 16000000UL
+#define BAUD 9600
+#define LED_PIN PB5
 
-void setup(){
-  DDRB |= (1 << led_pin);
+unsigned int ubrr = F_CPU / 16 / BAUD - 1;
 
- }
+int main(void) {
+  DDRB |= (1 << LED_PIN);  // Configura PB5 como saída
+  UBRR0H = ubrr >> 8;      // Configura a taxa de baud
+  UBRR0L = ubrr;
+  UCSR0B = (1 << RXEN0) | (1 << RXCIE0) | (1 << TXEN0);  // Habilita RX, TX e interrupção
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);                // 8 bits de dados
+  sei();  // Habilita interrupções globais
 
-int main()
+  while(1);  // Espera por interrupções
 
-{
-  setup();
+  return 0;
+}
 
-  PORTB &= ~(1<<led_pin);
- 
-
-
- return 0;
-
+ISR(USART_RX_vect) {
+  if (UDR0 == 'D') PORTB &= ~(1 << LED_PIN);  // Desliga LED
+  if (UDR0 == 'L') PORTB |= (1 << LED_PIN);   // Liga LED
 }
